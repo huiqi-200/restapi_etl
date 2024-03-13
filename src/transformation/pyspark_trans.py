b1d3b5386ae496f6cb41cb088adf681bbc2074e4
HQ <61664218+tinee200@users.noreply.github.com>
2024-03-13 21:41:09 +0800

    Initialize

--- src/transformation/pyspark_trans.py
+++ src/transformation/pyspark_trans.py
@@ -1,6 +1,7 @@
 # Import
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
+import pyspark.sql.functions as F
 
 from requests_weather import get_weather_forecast
 
@@ -10,8 +11,24 @@ spark = SparkSession.builder.getOrCreate()
 # Read CSV file into DataFrame
 json_weather = get_weather_forecast()
 
-rdd = spark.sparkContext.parallelize(json_weather)
-df = spark.read.json(rdd)
+def get_four_day_forecast_dataframe(json_weather = json_weather):
+    """Transforms the 
+    Args:
+        json_weather: Dictionary of json out
+
+     
+    
+    Returns:
+    ------------------------
+    
+    
+    """
+    dataframe = spark.createDataFrame(json_weather['items'][0]['forecasts']) 
+    pivot_df = dataframe.select(F.col('date'), F.col('forecast'), F.explode(F.col('temperature')).alias('temperature_low_high', 'temperature')) \
+            .groupBy("date", 'forecast').pivot("temperature_low_high").sum('temperature')
+
+    return pivot_df
 
 
-print(df)
+if __name__ == '__main__':
+    get_four_day_forecast_dataframe()
